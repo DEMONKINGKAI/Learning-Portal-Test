615c300d290932b8749ca1f862380cdd410b6a73
Archit <Archit@10.0.2.121>
2024-02-12 11:35:56 +0530

    controller

--- learning-portal/src/main/java/com/example/learningportal/controller/UserController.java
+++ learning-portal/src/main/java/com/example/learningportal/controller/UserController.java
@@ -2,7 +2,6 @@ package com.example.learningportal.controller;
 
 import com.example.learningportal.dto.UserDTO;
 import com.example.learningportal.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +14,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @Autowired
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
